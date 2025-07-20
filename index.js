52ab079bcad04e06f80be11543b948fe8e3af121
Anshul Gadia <anshulgadia04@gmail.com>
2025-07-20 15:04:52 +0530

    Redirect url changed

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'em
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('https://ai-newsroom-frontend.vercel.app/dashboard');
+    res.redirect('https://ai-newsroom-frontend-jqkpadmnx-anshulgadia04s-projects.vercel.app/dashboard');
   }
 );
 
@@ -44,7 +44,7 @@ app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] 
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('https://ai-newsroom-frontend.vercel.app/dashboard');
+    res.redirect('https://ai-newsroom-frontend-jqkpadmnx-anshulgadia04s-projects.vercel.app/dashboard');
   }
 );
 
